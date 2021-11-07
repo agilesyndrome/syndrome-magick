1eea39fa93c66cc7615eb1cdf2733d06561b81a0
Drew Easley <deasley@spoonflower.com>
2021-11-07 15:08:13 -0500

    updated timeouts

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -4,7 +4,7 @@ locals {
   image_bucket = var.image_bucket
   version = jsondecode(file("../src/package.json")).version
   timeout_seconds = 90
-  lambda_memory_mb = 1024
+  lambda_memory_mb = 1500
 }
 
 # Resource names, based on the values above
